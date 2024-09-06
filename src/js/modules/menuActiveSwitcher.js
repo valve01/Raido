d1d78a4f3c491bd49b718a6bcad8ea63b8fa241d
valve01 <118441831+valve01@users.noreply.github.com>
2024-09-06 13:39:52 +0300

    5

--- src/js/modules/menuActiveSwitcher.js
+++ src/js/modules/menuActiveSwitcher.js
@@ -1,12 +1,40 @@
 // const aboutSection = document.querySelector('.about');
-// const dataMenuItemAbout = document.querySelector('.data-menu-item-about');
+// const dataMenuItemAbout = document.querySelector(".menu-item a[data-menu-name='about']");
 
+// window.addEventListener('scroll', () => {
+// 	if (aboutSection.getBoundingClientRect().top <= 0) {
+// 		dataMenuItemAbout.classList.add('menu-item--active');
+// 	}
+// 	if(aboutSection.getBoundingClientRect().bottom <=0 || aboutSection.getBoundingClientRect().top > 0){
+// 		dataMenuItemAbout.classList.remove('menu-item--active');
+// 	}
+// });
 
+// ========================================================================================
 
+const allMenuItems = document.querySelectorAll('.header .menu-link');
 
+const sections = document.querySelectorAll('.section');
 
-
-
+window.addEventListener('scroll', () => {
+	// sections.forEach((item) => {console.log(item.getBoundingClientRect());
+	allMenuItems[0].classList.remove('menu-item--active');
+		const sectionName = item.dataset.sectionName;
+		allMenuItems.forEach((menuItem) => {
+			
+			if (item.getBoundingClientRect().top < 0 && menuItem.dataset.menuName == sectionName) {
+				
+				menuItem.classList.add('menu-item--active');
+			}
+			if (
+				(item.getBoundingClientRect().bottom < 0 || item.getBoundingClientRect().top > 0) &&
+				menuItem.dataset.menuName == sectionName
+			) {
+				menuItem.classList.remove('menu-item--active');
+			}
+		});
+	});
+});
 
 // ============================================================================================================
 // const allMenuItems = document.querySelectorAll('.header .menu-item');
