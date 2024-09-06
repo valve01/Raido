ff9c46e5673a76396a82a5922d3344d85c571c57
valve01 <118441831+valve01@users.noreply.github.com>
2024-09-06 15:57:49 +0300

    Доделал переключение меню по скроллу

--- src/js/modules/menuActiveSwitcher.js
+++ src/js/modules/menuActiveSwitcher.js
@@ -1,33 +1,15 @@
-// const aboutSection = document.querySelector('.about');
-// const dataMenuItemAbout = document.querySelector(".menu-item a[data-menu-name='about']");
-
-// window.addEventListener('scroll', () => {
-// 	if (aboutSection.getBoundingClientRect().top <= 0) {
-// 		dataMenuItemAbout.classList.add('menu-item--active');
-// 	}
-// 	if(aboutSection.getBoundingClientRect().bottom <=0 || aboutSection.getBoundingClientRect().top > 0){
-// 		dataMenuItemAbout.classList.remove('menu-item--active');
-// 	}
-// });
-
-// ========================================================================================
-
 const allMenuItems = document.querySelectorAll('.header .menu-link');
-
 const sections = document.querySelectorAll('.section');
 
 window.addEventListener('scroll', () => {
-	// sections.forEach((item) => {console.log(item.getBoundingClientRect());
-	allMenuItems[0].classList.remove('menu-item--active');
+	sections.forEach((item) => {
 		const sectionName = item.dataset.sectionName;
 		allMenuItems.forEach((menuItem) => {
-			
-			if (item.getBoundingClientRect().top < 0 && menuItem.dataset.menuName == sectionName) {
-				
+				if (item.getBoundingClientRect().top < 110 && menuItem.dataset.menuName == sectionName) {
 				menuItem.classList.add('menu-item--active');
 			}
 			if (
-				(item.getBoundingClientRect().bottom < 0 || item.getBoundingClientRect().top > 0) &&
+				(item.getBoundingClientRect().bottom < 110 || item.getBoundingClientRect().top >= 110) &&
 				menuItem.dataset.menuName == sectionName
 			) {
 				menuItem.classList.remove('menu-item--active');
@@ -36,21 +18,3 @@ window.addEventListener('scroll', () => {
 	});
 });
 
-// ============================================================================================================
-// const allMenuItems = document.querySelectorAll('.header .menu-item');
-// const sections = document.querySelectorAll('.section');
-
-// window.addEventListener('scroll', () => {
-// 	sections.forEach((item) => {
-// 		if ((item.getBoundingClientRect().y <= 0)) {
-// 			const sectionName = item.dataset.sectionName;
-// 			console.log(sectionName)
-// 			allMenuItems.forEach((menuItem) => {
-// 				menuItem.classList.remove('menu-item--active');
-// 				if (menuItem.dataset.menuName === sectionName) {
-// 					menuItem.classList.add('menu-item--active');
-// 				}
-// 			});
-// 		}
-// 	});
-// });
