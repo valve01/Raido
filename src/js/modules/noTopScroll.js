1f1ac982168a978b3dfe01c0eb342c23a0a00bde
valve01 <118441831+valve01@users.noreply.github.com>
2024-09-18 21:38:54 +0300

    Адаптация готова. Вроде все поправил. Осталась мобильная навигация

--- src/js/modules/noTopScroll.js
+++ src/js/modules/noTopScroll.js
@@ -1,4 +1,7 @@
-const journalsLinks = document.querySelectorAll('.journals a');
+const journalsLinks = document.querySelectorAll(
+	'.journals a, .header__search-link, .explore-link, .read-link, .discover__item-link, .discover-link, .blog__link, .galery a, .mail-form__contacts-inner a, .footer__social-block a ',
+);
+// console.log(journalsLinks);
 journalsLinks.forEach((link) => {
 	link.addEventListener('click', function (e) {
 		e.preventDefault();
