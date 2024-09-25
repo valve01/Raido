7df0172fbe8fca60953e00b92fea5f2114ef1f12
valve01 <118441831+valve01@users.noreply.github.com>
2024-09-25 10:18:18 +0300

    Update 2024-09-25T07:18:05.898Z

--- src/js/modules/noTopScroll.js
+++
@@ -1,9 +0,0 @@
-const journalsLinks = document.querySelectorAll(
-	'.journals a, .header__search-link, .explore-link, .read-link, .discover__item-link, .discover-link, .blog__link, .galery a, .mail-form__contacts-inner a, .footer__social-block a ',
-);
-// console.log(journalsLinks);
-journalsLinks.forEach((link) => {
-	link.addEventListener('click', function (e) {
-		e.preventDefault();
-	});
-});
