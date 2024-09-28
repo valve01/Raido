77080487b77f286ecf61631385837da02f53b594
valve01 <118441831+valve01@users.noreply.github.com>
2024-09-28 20:07:42 +0300

    форма по всем кнопкам

--- src/js/modules/showHideForm.js
+++ src/js/modules/showHideForm.js
@@ -1,6 +1,8 @@
 function showHideForm() {
 	const contacts = document.querySelector('.contacts');
-	const contactsCircleBlockMail = document.querySelector('.contacts__circle-block-mail');
+	const formActivatorBtns = document.querySelectorAll(
+		'.contacts__circle-block-mail, .explore-btn, div.read-btn, .discover-btn ',
+	);
 	const mailForm = document.querySelector('.mail-form');
 	const mailFormSubmit = document.querySelector('.mail-form__submit');
 
@@ -8,10 +10,14 @@ function showHideForm() {
 		block.scrollIntoView(top);
 	};
 
-	contactsCircleBlockMail.addEventListener('click', (e) => {
-		mailForm.classList.toggle('mail-form--active');
-		setTimeout(() => scrollToBlock(mailForm), 500);
-		e.preventDefault();
+	formActivatorBtns.forEach((btn) => {
+		btn.addEventListener('click', (e) => {
+			mailForm.classList.toggle('mail-form--active');
+			if (mailForm.classList.contains('mail-form--active')) {
+				setTimeout(() => scrollToBlock(mailForm), 500);
+			}
+			e.preventDefault();
+		});
 	});
 
 	mailFormSubmit.addEventListener('click', (e) => {
