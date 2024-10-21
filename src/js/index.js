d87ed87f64ff3be7d51488ccce5c21bb245dd08c
valve01 <118441831+valve01@users.noreply.github.com>
2024-10-21 13:29:36 +0300

    l

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ import './modules/scrollToSection';
 import './modules/noTopScroll';
 import './modules/baguettebox';
 import './modules/menuActiveSwitcher';
-import './modules/sweetAlarm';
+// import './modules/sweetAlarm';
 import './modules/validator';
 
 import mobileNav from './modules/mobile-nav';
